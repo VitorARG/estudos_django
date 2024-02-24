78fbee78f29ace61959c5fe4ba0f0f0935b643c6
VitorARG <vitor.augusto.6992@gmail.com>
2024-02-24 03:47:17 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):  # pylint: disable=unused-argument
     return render(request, 'recipes/home.html', context={'name': 'Vitor'})
-
-
-def sobre(request):  # pylint: disable=unused-argument
-    return render(request, 'temp.html', context={'name': 'Vitor'})
-
-
-def contato(request):  # pylint: disable=unused-argument
-    return HttpResponse('contato 3')
