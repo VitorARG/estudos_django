eec0d82ab41894003831632a447e07196a042fa7
VitorARG <vitor.augusto.6992@gmail.com>
2024-03-06 03:50:40 -0300

    adicionado uma nova view, url e arquivo recipe view

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):  # pylint: disable=unused-argument
-    return render(request, 'recipes/pages/home.html', context={'name': 'Vitor'})
+    return render(request, 'recipes/pages/home.html')
+
+
+def recipe(request, id):  # pylint: disable=unused-argument
+    return render(request, 'recipes/pages/recipe-view.html')
