120fc9cf4d105aecf1d6f4b8583c3ddace1fe97e
VitorARG <vitor.augusto.6992@gmail.com>
2024-03-07 03:45:50 -0300

    correções de testes

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
+from util.recipes.factory import make_recipe
+
 
 def home(request):  # pylint: disable=unused-argument
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
 
 
 def recipe(request, id):  # pylint: disable=unused-argument
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe
+    })
