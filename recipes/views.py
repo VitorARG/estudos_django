be08a334a6925f44fa131f08c18cf5f7e179756b
VitorARG <vitor.augusto.6992@gmail.com>
2024-04-29 02:17:42 -0300

    Alteraçoes necessarias category

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render
 
 from util.recipes.factory import make_recipe
 
+from .models import Recipe
+
 
 def home(request):  # pylint: disable=unused-argument
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
     })
 
 
