f002644c1aaccad79fac72be53027d304b0496dd
VitorARG <vitor.augusto.6992@gmail.com>
2024-04-30 03:06:10 -0300

    trocando mensagem de erro por get_list_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_list_or_404, render
 
 from util.recipes.factory import make_recipe
 
@@ -13,10 +14,21 @@ def home(request):  # pylint: disable=unused-argument
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('-id')
+    # recipes = Recipe.objects.filter(
+    # category__id=category_id,
+    # is_published=True,
+    # ).order_by('-id')
+
+    # if not recipes:
+    # raise Http404('Not found 🥲')
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True
+    ).order_by('-id')
+    )
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category |'
     })
 
 
