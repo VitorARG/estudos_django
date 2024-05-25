4fbf87e4abd594e69441040d9d3363147c4d6c0b
VitorARG <vitor.augusto.6992@gmail.com>
2024-05-25 04:12:12 -0300

    Criando testes parametrizados com parameterized

--- tests/test_recipe_models_recipe.py
+++ tests/test_recipe_models_recipe.py
@@ -1,5 +1,6 @@
 
 from django.forms import ValidationError
+from parameterized import parameterized
 
 from .test_recipe_base import Recipetestbase
 
@@ -9,14 +10,13 @@ class RecipeModelsTest(Recipetestbase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_recipe_fields_max_lenght(self):
-        fields = [
-            ('title', 65),
-            ('description', 65),
-            (' preparation_time_unit', 65),
-            (' servings_unit', 65),
-        ]
-        for field, maxlengt in fields:
-            setattr(self.recipe, field, 'a' * (maxlengt + 1))
-            with self.assertRaises(ValidationError):
-                self.recipe.full_clean()
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_lenght(self, field, maxlength):
+        setattr(self.recipe, field, 'A' * (maxlength + 1))
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
