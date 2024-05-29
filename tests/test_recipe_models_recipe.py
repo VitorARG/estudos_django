0272141e7df333b388fd3ffe5b90891c9efe597a
VitorARG <vitor.augusto.6992@gmail.com>
2024-05-29 18:58:37 -0300

    Testando a string representation da Recipe

--- tests/test_recipe_models_recipe.py
+++ tests/test_recipe_models_recipe.py
@@ -49,3 +49,9 @@ class RecipeModelsTest(Recipetestbase):
         self.assertFalse(recipe.is_published,
                          msg='Recipe is_published is not False',
                          )
+
+    def test_recipe_string_representation(self):
+        self.recipe.title = 'Testing representation'
+        self.recipe.full_clean()
+        self.recipe.save()
+        self.assertEqual(str(self.recipe), 'Testing representation')
