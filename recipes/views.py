204dd6d51824db629cf5da925860208ca558178e
VitorARG <vitor.augusto.6992@gmail.com>
2024-06-11 03:04:48 -0300

    template search

--- recipes/views.py
+++ recipes/views.py
@@ -34,7 +34,13 @@ def recipe(request, id):  # pylint: disable=unused-argument
 
 
 def search(request):
-    search_term = request.GET.get('q')
+    search_term = request.GET.get('q', '').strip()
+
     if not search_term:
         raise Http404
-    return render(request, 'recipes/pages/search.html')
+
+    return render(request, 'recipes/pages/search.html', {
+                  'page_title': f'search for "{search_term}"',
+                  'search_term': search_term,
+
+                  })
