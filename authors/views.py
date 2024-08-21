425935b6756d0b39a179c5ee2889b580e09f7690
VitorARG <vitor.augusto.6992@gmail.com>
2024-08-21 01:48:22 -0300

    adicionando register_create

--- authors/views.py
+++ authors/views.py
@@ -1,15 +1,26 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import redirect, render
 
 from .forms import RegisterForm
 
 
 # Create your views here.
 def register_view(request):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    resgister_form_data = request.session.get('resgister_form_data', None)
+    form = RegisterForm(resgister_form_data)
 
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404
+
+    POST = request.POST
+    request.session['resgister_form_data'] = POST
+
+    form = RegisterForm(request.POST)
+
+    return redirect('authors:register')
